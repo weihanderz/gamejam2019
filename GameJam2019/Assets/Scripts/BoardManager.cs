dcc7d46d03bbf98c60f3b92652138ce164b64413
weihanderz <wei.protein@gmail.com>
2019-01-26 15:32:55 -0800

    added board manager

---
+++ GameJam2019/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic; 		//Allows us to use Lists.
+using Random = UnityEngine.Random; 		//Tells Random to use the Unity Engine random number generator.
+
+namespace Completed
+	
+{
+	
+	public class BoardManager : MonoBehaviour
+	{
+		// Using Serializable allows us to embed a class with sub properties in the inspector.
+		[Serializable]
+		public class Count
+		{
+			public int minimum; 			//Minimum value for our Count class.
+			public int maximum; 			//Maximum value for our Count class.
+			
+			
+			//Assignment constructor.
+			public Count (int min, int max)
+			{
+				minimum = min;
+				maximum = max;
+			}
+		}
+		public int columns = 8;
+		public int rows = 8;
+		public Count wallCount = new Count (5,9);
+		public GameObject[] floorTiles;
+		public GameObject[] outerWallTiles;
+		private Transform boardHolder;
+		private List <Vector3> gridPositions = new List <Vector3> ();
+
+
+		void InitialiseList ()
+		{
+			//Clear our list gridPositions.
+			gridPositions.Clear ();
+			
+			//Loop through x axis (columns).
+			for(int x = 1; x < columns-1; x++)
+			{
+				//Within each column, loop through y axis (rows).
+				for(int y = 1; y < rows-1; y++)
+				{
+					//At each index add a new Vector3 to our list with the x and y coordinates of that position.
+					gridPositions.Add (new Vector3(x, y, 0f));
+				}
+			}
+		}
+		void BoardSetup ()
+		{
+			boardHolder = new GameObject("Board").transform;
+			//Loop along x axis, starting from -1 (to fill corner) with floor or outerwall edge tiles.
+			for(int x = -1; x < columns + 1; x++)
+			{
+				//Loop along y axis, starting from -1 to place floor or outerwall tiles.
+				for(int y = -1; y < rows + 1; y++)
+				{
+					//Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
+					GameObject toInstantiate = floorTiles[Random.Range (0,floorTiles.Length)];
+					
+					//Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
+					if(x == -1 || x == columns || y == -1 || y == rows)
+						toInstantiate = outerWallTiles[Random.Range (0, outerWallTiles.Length)];
+					
+					//Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
+					GameObject instance =
+						Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
+					
+					//Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
+					instance.transform.SetParent (boardHolder);
+				}
+			}
+		}		
+		
+		//SetupScene initializes our level and calls the previous functions to lay out the game board
+		public void SetupScene (int level)
+		{
+			//Creates the outer walls and floor.
+			BoardSetup ();
+
+		}
+	}
+}
--- GameJam2019/Assets/Scripts/GameManager.cs
+++ GameJam2019/Assets/Scripts/GameManager.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+namespace Completed
+{
+	using System.Collections.Generic;
+	using UnityEngine.UI;
+    public class GameManager : MonoBehaviour
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        private BoardManager boardScript;
+        // Use this for initialization
+        void Start()
+        {
+            boardScript = GetComponent<BoardManager>();
+            boardScript.SetupScene(1);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
 }
