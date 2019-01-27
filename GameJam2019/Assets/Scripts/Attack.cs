ccb199723b25a8de99fea620fc1dbd2ddaa80aad
Nathan <nathmclean@gmail.com>
2019-01-27 15:37:38 -0800

    better enemy attacks
    - add optional attack sprite effects
    - add enemy attack cooldown
    - tweak enemy numbers

--- GameJam2019/Assets/Scripts/Attack.cs
+++ GameJam2019/Assets/Scripts/Attack.cs
@@ -13,12 +13,17 @@ public class Attack : MonoBehaviour {
 
     private Collider2D attackCollider;
     private Nullable<float> attackTime = null;
+    private SpriteRenderer attackSprite;
 
 	// Use this for initialization
 	void Start () {
         this.attackCollider = this.GetComponent<Collider2D>();
+        this.attackSprite = this.GetComponent<SpriteRenderer>();
         this.attackCollider.enabled = false;
         this.attackCollider.isTrigger = true;
+
+        if(this.attackSprite)
+            this.attackSprite.enabled = false;
 	}
 
     public void Execute()
@@ -37,11 +42,15 @@ public class Attack : MonoBehaviour {
                 this.attackTime + this.attackDelay <= Time.time
             ) {
                 this.attackCollider.enabled = true;
+                if(this.attackSprite)
+                    this.attackSprite.enabled = true;
             } else if (
                 this.attackCollider.enabled &&
                 this.attackTime + this.attackDelay + this.attackDuration <= Time.time
             ) {
                 this.attackCollider.enabled = false;
+                if(this.attackSprite)
+                    this.attackSprite.enabled = false;
                 this.attackTime = null;
             }
         }
