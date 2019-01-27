cd56b4901640071863f5c1278874850248018f1f
weihanderz <wei.protein@gmail.com>
2019-01-26 17:50:00 -0800

    added random walls

--- GameJam2019/Assets/Scripts/BoardManager.cs
+++ GameJam2019/Assets/Scripts/BoardManager.cs
@@ -24,15 +24,20 @@ namespace Completed
 				maximum = max;
 			}
 		}
-		public int columns = 8;
-		public int rows = 8;
-		public Count wallCount = new Count (5,9);
-		public GameObject[] floorTiles;
-		public GameObject[] outerWallTiles;
-		private Transform boardHolder;
-		private List <Vector3> gridPositions = new List <Vector3> ();
-
-
+		
+		
+		public int columns = 80; 										//Number of columns in our game board.
+		public int rows = 80;											//Number of rows in our game board.
+		public Count wallCount = new Count (40, 50);						//Lower and upper limit for our random number of walls per level.
+		public GameObject[] floorTiles;									//Array of floor prefabs.
+		public GameObject[] wallTiles;									//Array of wall prefabs.
+		public GameObject[] outerWallTiles;								//Array of outer tile prefabs.
+		
+		private Transform boardHolder;									//A variable to store a reference to the transform of our Board object.
+		private List <Vector3> gridPositions = new List <Vector3> ();	//A list of possible locations to place tiles.
+		
+		
+		//Clears our list gridPositions and prepares it to generate a new board.
 		void InitialiseList ()
 		{
 			//Clear our list gridPositions.
@@ -49,9 +54,14 @@ namespace Completed
 				}
 			}
 		}
+		
+		
+		//Sets up the outer walls and floor (background) of the game board.
 		void BoardSetup ()
 		{
-			boardHolder = new GameObject("Board").transform;
+			//Instantiate Board and set boardHolder to its transform.
+			boardHolder = new GameObject ("Board").transform;
+			
 			//Loop along x axis, starting from -1 (to fill corner) with floor or outerwall edge tiles.
 			for(int x = -1; x < columns + 1; x++)
 			{
@@ -63,7 +73,7 @@ namespace Completed
 					
 					//Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
 					if(x == -1 || x == columns || y == -1 || y == rows)
-						toInstantiate = outerWallTiles[Random.Range (0, outerWallTiles.Length)];
+						toInstantiate = outerWallTiles [Random.Range (0, outerWallTiles.Length)];
 					
 					//Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
 					GameObject instance =
@@ -73,13 +83,58 @@ namespace Completed
 					instance.transform.SetParent (boardHolder);
 				}
 			}
-		}		
+		}
+		
+		
+		//RandomPosition returns a random position from our list gridPositions.
+		Vector3 RandomPosition ()
+		{
+			//Declare an integer randomIndex, set it's value to a random number between 0 and the count of items in our List gridPositions.
+			int randomIndex = Random.Range (0, gridPositions.Count);
+			
+			//Declare a variable of type Vector3 called randomPosition, set it's value to the entry at randomIndex from our List gridPositions.
+			Vector3 randomPosition = gridPositions[randomIndex];
+			
+			//Remove the entry at randomIndex from the list so that it can't be re-used.
+			gridPositions.RemoveAt (randomIndex);
+			
+			//Return the randomly selected Vector3 position.
+			return randomPosition;
+		}
+		
+		
+		//LayoutObjectAtRandom accepts an array of game objects to choose from along with a minimum and maximum range for the number of objects to create.
+		void LayoutObjectAtRandom (GameObject[] tileArray, int minimum, int maximum)
+		{
+			//Choose a random number of objects to instantiate within the minimum and maximum limits
+			int objectCount = Random.Range (minimum, maximum+1);
+			
+			//Instantiate objects until the randomly chosen limit objectCount is reached
+			for(int i = 0; i < objectCount; i++)
+			{
+				//Choose a position for randomPosition by getting a random position from our list of available Vector3s stored in gridPosition
+				Vector3 randomPosition = RandomPosition();
+				
+				//Choose a random tile from tileArray and assign it to tileChoice
+				GameObject tileChoice = tileArray[Random.Range (0, tileArray.Length)];
+				
+				//Instantiate tileChoice at the position returned by RandomPosition with no change in rotation
+				Instantiate(tileChoice, randomPosition, Quaternion.identity);
+			}
+		}
+		
 		
 		//SetupScene initializes our level and calls the previous functions to lay out the game board
 		public void SetupScene (int level)
 		{
 			//Creates the outer walls and floor.
 			BoardSetup ();
+			
+			//Reset our list of gridpositions.
+			InitialiseList ();
+			
+			//Instantiate a random number of wall tiles based on minimum and maximum, at randomized positions.
+			LayoutObjectAtRandom (wallTiles, wallCount.minimum, wallCount.maximum);
 
 		}
 	}
